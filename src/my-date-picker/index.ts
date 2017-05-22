bb0f1a64beddf111479d0ca886b85efb09eedfa9
amol.deole <amol.deole@tudip.nl>
2017-05-22 23:25:30 +0530

    Added new changes for my date picker to make it installable dependency.

---
+++ src/my-date-picker/index.ts
@@ -0,0 +1,5 @@
+export * from './services/my-date-picker.locale.service';
+export * from './services/my-date-picker.date.validator.service';
+export * from './my-date-picker.component';
+export * from './my-date-picker.module'
+export * from './interfaces/index';
